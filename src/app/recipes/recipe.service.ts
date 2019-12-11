7134fbee2134a66a43f637f0a9d792dd66b58d7a
Andreas <alpaca@mailbox.org>
2019-12-11 21:00:40 +0100

    route

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
-import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
-import { Ingredient } from "../shared/ingredient.model";
+import { Recipe } from './recipe.model';
+import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
+@Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    private recipes: Recipe[] = [
+  recipesChanged = new Subject<Recipe[]>();
+
+  private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
             new Ingredient('Meat', 1),
             new Ingredient('French Fries', 20)
@@ -14,7 +18,27 @@ export class RecipeService {
             new Ingredient('Semmel', 1)
         ])
       ];
+    constructor(private slService: ShoppingListService) {}
+
     getRecipes() {
         return this.recipes.slice();
+    }
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
+    addIngredientsToSL(ingredients: Ingredient[]) {
+        this.slService.addIngrToSl(ingredients);
+    }
+    addRecipe(recipe: Recipe) {
+      this.recipes.push(recipe);
+      this.recipesChanged.next(this.recipes.slice());
+    }
+    updateRecipe(index: number, newRecipe: Recipe) {
+      this.recipes[index] = newRecipe;
+      this.recipesChanged.next(this.recipes.slice());
+    }
+    deleteRecipe(index: number) {
+      this.recipes.splice(index, 1);
+      this.recipesChanged.next(this.recipes.slice());
     }
 }

d646c35a0b16c60c52d0ff2a3c154c02feea7190
Andreas <alpaca@mailbox.org>
2019-12-21 20:40:39 +0100

    2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
-import { DataStorageService } from '../shared/data-storage.service';
-import { AuthService } from '../auth/auth.service';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html"
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   isAuth = false;
   private userSub: Subscription;
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.userSub = this.authService.user.subscribe(user => {
       this.isAuth = !!user;
-      console.log(!user)});
+      console.log(!user);
+    });
+  }
+  onLogout() {
+    this.authService.logout();
   }
 
-  onSaveData(){
+  onSaveData() {
     this.dataStorageService.storeRecipes();
   }
-  onRestoreData(){
+  onRestoreData() {
     this.dataStorageService.restoreRecipes().subscribe();
   }
   ngOnDestroy() {
     this.userSub.unsubscribe();
-  }}
+  }
+}
